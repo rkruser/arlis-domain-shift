9e28e996a8342b6f49824ceeb10778bd471c4729
Ryen Krusinga <rkrusinga@gmail.com>
2022-03-03 12:49:45 -0500

    Still works when not trained on planes

--- ae_method.py
+++ ae_method.py
@@ -8,6 +8,9 @@ import sys
 import os
 
 
+
+
+
 from utils import EasyDict as edict
 
 
@@ -450,7 +453,89 @@ def view_extracted_probabilities(model_path, model_name_prefix, data_cfg):
     
     
     
+def view_extracted_probabilities_combined(model_path, model_name_prefix, data_cfg):
+    # note the off-manifold scores are norms here, not squared norms
     
+    #data = pickle.load(open('./models/autoencoder/extracted_info_exp_4.pkl', 'rb'))
+    data = pickle.load(open(data_cfg.plot_stage.prob_sample_file,'rb'))
+
+    ######### View top and bottom of each ###########3
+    real_images = torch.load(data_cfg.visualize_stage.real)[1]['test']['images']
+    fake_images = torch.load(data_cfg.visualize_stage.fake)[1]['test']['images']
+    aug_images = torch.load(data_cfg.visualize_stage.augmented)[0]['test']['images']
+
+    real_znorms = data['real']['z_norms']
+    real_e2z = data['real']['e2z_jacobian_probs']
+    real_z2e = data['real']['z2e_jacobian_probs']
+
+    fake_znorms = data['fake']['z_norms']
+    fake_e2z = data['fake']['e2z_jacobian_probs']
+    fake_z2e = data['fake']['z2e_jacobian_probs']
+
+    aug_znorms = data['augmented']['z_norms']
+    aug_e2z = data['augmented']['e2z_jacobian_probs']
+    aug_z2e = data['augmented']['z2e_jacobian_probs']
+
+    view_top_and_bottom(real_znorms, real_images, 'Real cars, z norms')
+    view_top_and_bottom(real_e2z, real_images, 'Real cars, e2z')
+    view_top_and_bottom(real_z2e, real_images, 'Real cars, z2e')
+
+    view_top_and_bottom(fake_znorms, fake_images, 'Fake cars, z norms')
+    view_top_and_bottom(fake_e2z, fake_images, 'Fake cars, e2z')
+    view_top_and_bottom(fake_z2e, fake_images, 'Fake cars, z2e')
+
+    view_top_and_bottom(aug_znorms, aug_images, 'Real planes, z norms')
+    view_top_and_bottom(aug_e2z, aug_images, 'Real planes, e2z')
+    view_top_and_bottom(aug_z2e, aug_images, 'Real planes, z2e')
+
+    ###################
+
+    
+    
+    plt.title("logpriors")
+    plt.hist(data.real.log_priors.numpy(), bins=50, density=True, alpha=0.3, label="Real cars")
+    plt.hist(data.fake.log_priors.numpy(), bins=50, density=True, alpha=0.3, label="Fake cars")    
+    plt.hist(data.augmented.log_priors.numpy(), bins=50, density=True, alpha=0.3, label="Real planes")
+    plt.legend()
+    plt.show()
+    
+    plt.title("e2z jacobians")
+    plt.hist(data.real.e2z_jacobian_probs.numpy(), bins=50, density=True, alpha=0.3, label="Real cars")
+    plt.hist(data.fake.e2z_jacobian_probs.numpy(), bins=50, density=True, alpha=0.3, label="Fake cars")    
+    plt.hist(data.augmented.e2z_jacobian_probs.numpy(), bins=50, density=True, alpha=0.3, label="Real planes")
+    plt.legend()
+    plt.show()    
+    
+    
+    plt.title("e2z combined")
+    plt.hist(data.real.total_e2z_probs.numpy(), bins=50, density=True, alpha=0.3, label="Real cars")
+    plt.hist(data.fake.total_e2z_probs.numpy(), bins=50, density=True, alpha=0.3, label="Fake cars")    
+    plt.hist(data.augmented.total_e2z_probs.numpy(), bins=50, density=True, alpha=0.3, label="Real planes")
+    plt.legend()
+    plt.show()       
+    
+    plt.title("z2e jacobians")
+    plt.hist(data.real.z2e_jacobian_probs.numpy(), bins=50, density=True, alpha=0.3, label="Real cars")
+    plt.hist(data.fake.z2e_jacobian_probs.numpy(), bins=50, density=True, alpha=0.3, label="Fake cars")    
+    plt.hist(data.augmented.z2e_jacobian_probs.numpy(), bins=50, density=True, alpha=0.3, label="Real planes")
+    plt.legend()
+    plt.show()     
+    
+    
+    plt.title("z2e combined")
+    plt.hist(data.real.total_z2e_probs.numpy(), bins=50, density=True, alpha=0.3, label="Real cars")
+    plt.hist(data.fake.total_z2e_probs.numpy(), bins=50, density=True, alpha=0.3, label="Fake cars")    
+    plt.hist(data.augmented.total_z2e_probs.numpy(), bins=50, density=True, alpha=0.3, label="Real planes")
+    plt.legend()
+    plt.show()     
+    
+    plt.title("z norms")
+    plt.hist(data.real.z_norms.cpu().numpy(), bins=50, density=True, alpha=0.3, label="Real cars")
+    plt.hist(data.fake.z_norms.cpu().numpy(), bins=50, density=True, alpha=0.3, label="Fake cars")    
+    plt.hist(data.augmented.z_norms.cpu().numpy(), bins=50, density=True, alpha=0.3, label="Real planes")
+    plt.legend()
+    plt.show()      
+   
     
     
     
@@ -866,8 +951,8 @@ if __name__ == '__main__':
     train_cfg = train_config(opt.train_config_key)
     
 
-    print("Data config")
-    print(data_cfg)
+#    print("Data config")
+#    print(data_cfg)
     
     if opt.mode == 'train_ae':
         build_and_train_autoencoder(opt.model_path, opt.model_name_prefix, autoencoder_cfg, data_cfg, train_cfg)        
@@ -908,7 +993,8 @@ if __name__ == '__main__':
         from ae_combined import visualize_model_combined
         visualize_model_combined(opt.model_path, opt.model_name_prefix, data_cfg)
     elif opt.mode == 'plot_probs_combined':
-        from ae_combined import view_extracted_probabilities_combined
+#        from ae_combined import view_extracted_probabilities_combined
+        from ae_combined import view_top_and_bottom
         view_extracted_probabilities_combined(opt.model_path, opt.model_name_prefix, data_cfg)
 
            
